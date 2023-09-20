e37874aae398cab357468a257f0dbf3f81395624
Wojciech Rusiecki <miecze123@onet.pl>
2023-09-20 03:24:30 +0200

    Add project files.

---
+++ WindowsForm CSV Reader/Models/DataModel.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CsvHelper.Configuration.Attributes;
+namespace WindowsForm_CSV_Reader.Models
+{
+    public class DataModel
+    {
+        [Name("zdjecie")]
+        public string Main_Picture { get; set; }
+
+        [Name("zdjęcie")]
+        public string Pictures { get; set;}
+
+        [Name("KOD")]
+        public string ID_KARTOTEKI { get; set; }
+
+        [Name("nazwa")]
+        public string NAZWA { get; set; }
+
+        [Name("EAN")]
+        public string EAN { get; set; }
+
+        [Name("Producent")]
+        public string PRODUCENT { get; set; }
+
+        [Name("atrybut stan unimet")]
+        public string STAN_NA_MAGAZYNIE { get; set; }
+
+        [Name("Vat")]
+        public string VAT { get; set; }
+
+        [Name("waga")]
+        public string WAGA { get; set; }
+
+        [Name("jm")]
+        public string JEDNOSTKA_MIARY { get; set; }
+
+        [Name("opis html")]
+        public string OPIS { get; set; }
+
+        [Name("atrybut unimet min zamówieniowe")]
+        public string MIN_ZAM { get; set; }
+
+        [Name("nr katalogowy")]
+        public string KOD_PRODUCENTA { get; set; }
+
+    }
+}
